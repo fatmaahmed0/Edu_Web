fa42979a0a394a2f9d180839dde9d2f07a7d9864
fatmaahmed0 <fatmaahmed3255@gmail.com>
2025-01-29 02:26:21 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {animation: {
+      ping: 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite', bounce : "bounce 2s infinite"
+    },},
+  },
+  plugins: [require('daisyui'),require('flowbite/plugin')],
+  
+}
+
